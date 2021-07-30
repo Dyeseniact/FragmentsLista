9857aed158cb8231b8fb7a44e648824e586fe8d0
Dyeseniact <camposdyes@gmail.com>
2021-07-29 22:14:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fragmentslista/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentslista
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    //definir una variable estatica
+    companion object {
+        val PRODUCT = "PRODUCT"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_activity)
+
+        val product = intent.getParcelableExtra<Product>(PRODUCT)
+        val detailFragment = supportFragmentManager.findFragmentById(R.id.fragmentDetail) as? DetailFragment
+        if (product != null) {
+            detailFragment?.showProduct(product)
+        }
+
+    }
+}
